64c0666e52746d0f5649770b28bdb9c9283c02cf
Vincent Nwonah <vnw@nrd.lt>
2019-10-10 16:42:57 +0300

    added syncfus

--- src/XFI/XFI/XFIMainWindowControl.xaml.cs
+++ src/XFI/XFI/XFIMainWindowControl.xaml.cs
@@ -7,7 +7,9 @@
     using System.Windows.Media;
     using XFI.Models;
     using XFI.Services;
-
+    using Syncfusion.Windows.Tools.Controls;
+    using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Interaction logic for XFIMainWindowControl.
@@ -15,13 +17,20 @@
     public partial class XFIMainWindowControl : UserControl
     {
         private readonly FontSearchService _fontSearchService;
+        private List<string> _fontList;
         /// <summary>
         /// Initializes a new instance of the <see cref="XFIMainWindowControl"/> class.
         /// </summary>
         public XFIMainWindowControl()
         {
             _fontSearchService = new FontSearchService();
+            _fontList = new List<string>() {"boy", "girl", "child" };
             this.InitializeComponent();
+            this.AutoComplete.CustomSource = _fontList;
+
+
+
+
         }
 
         /// <summary>
@@ -40,76 +49,81 @@
 
         private async void TextBox_KeyUp(object sender, KeyEventArgs e)
        {
-            bool found = false;
-            var border = (resultStack.Parent as ScrollViewer).Parent as Border;
+            //bool found = false;
+            //var border = (resultStack.Parent as ScrollViewer).Parent as Border;
 
-            string query = (sender as TextBox).Text;
-            if (query.Length == 0)
-            {
-                // Clear
-                resultStack.Children.Clear();
-                border.Visibility = System.Windows.Visibility.Collapsed;
-            }
-            else
-            {
-                border.Visibility = System.Windows.Visibility.Visible;
-            }
+            //string query = (sender as TextBox).Text;
+            //if (query.Length == 0)
+            //{
+            //    // Clear
+            //    resultStack.Children.Clear();
+            //    border.Visibility = System.Windows.Visibility.Collapsed;
+            //}
+            //else
+            //{
+            //    border.Visibility = System.Windows.Visibility.Visible;
+            //}
 
-            var data = await _fontSearchService.FetchFontListAsync(query);
+            //var data = await _fontSearchService.FetchFontListAsync(query);
 
             
 
-            // Clear the list
-            resultStack.Children.Clear();
+            //// Clear the list
+            //resultStack.Children.Clear();
 
-            // Add the result
-            if(data is object)
-            {
-                foreach (var obj in data)
-                {
-                    addItem(obj.Name);
-                    found = true;
-                }
-            }
+            //// Add the result
+            //if(data is object)
+            //{
+            //    foreach (var obj in data)
+            //    {
+            //        addItem(obj.Name);
+            //        found = true;
+            //    }
+            //}
           
 
-            if (!found)
-            {
-                resultStack.Children.Add(new TextBlock() { Text = "No results found." });
-            }
+            //if (!found)
+            //{
+            //    resultStack.Children.Add(new TextBlock() { Text = "No results found." });
+            //}
         }
 
         private void addItem(string text)
         {
-            TextBlock block = new TextBlock();
+            //TextBlock block = new TextBlock();
 
-            // Add the text
-            block.Text = text;
+            //// Add the text
+            //block.Text = text;
 
-            // A little style...
-            block.Margin = new Thickness(2, 3, 2, 3);
-            block.Cursor = Cursors.Hand;
+            //// A little style...
+            //block.Margin = new Thickness(2, 3, 2, 3);
+            //block.Cursor = Cursors.Hand;
 
-            // Mouse events
-            block.MouseLeftButtonUp += (sender, e) =>
-            {
-                textBox.Text = (sender as TextBlock).Text;
-            };
+            //// Mouse events
+            //block.MouseLeftButtonUp += (sender, e) =>
+            //{
+            //    textBox.Text = (sender as TextBlock).Text;
+            //};
 
-            block.MouseEnter += (sender, e) =>
-            {
-                TextBlock b = sender as TextBlock;
-                b.Background = Brushes.PeachPuff;
-            };
+            //block.MouseEnter += (sender, e) =>
+            //{
+            //    TextBlock b = sender as TextBlock;
+            //    b.Background = Brushes.PeachPuff;
+            //};
 
-            block.MouseLeave += (sender, e) =>
-            {
-                TextBlock b = sender as TextBlock;
-                b.Background = Brushes.Transparent;
-            };
+            //block.MouseLeave += (sender, e) =>
+            //{
+            //    TextBlock b = sender as TextBlock;
+            //    b.Background = Brushes.Transparent;
+            //};
 
-            // Add to the panel
-            resultStack.Children.Add(block);
+            //// Add to the panel
+            //resultStack.Children.Add(block);
+        }
+
+        private void AutoComplete_KeyUp(object sender, KeyEventArgs e)
+        {
+            _fontList.Add("amechi");
         }
     }
 }
